2bcd7ee265965dce2b0f219edd1a70bf688ad39d
rodriguetech <rodriguet802@gmail.com>
2023-01-01 17:19:20 +0100

    Initialisation

---
+++ src/Endpoints.php
@@ -0,0 +1,8 @@
+<?php
+
+
+
+final class Endpoints
+{
+    const BASE_URL = "https://api.goxens.com";
+}
---
+++ src/byBearerToken/Auth.php
@@ -0,0 +1,20 @@
+<?php
+
+use GuzzleHttp\Client;
+require_once '../Endpoints.php';
+
+
+class Auth {
+
+    private $client;
+
+    public function __construct()
+    {
+        $this->client = new Client();
+    }
+
+
+
+
+
+}
